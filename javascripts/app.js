bab695e7dea3c543bdd3b55be472c297600f32a5
Ivan Tilikin <it@poloniumarts.com>
2014-11-26 12:17:55 +0300

    [upd] html5 mode

--- javascripts/app.js
+++ javascripts/app.js
@@ -10,70 +10,70 @@ var App = angular.module('App', [
 
 App.config(['$routeProvider', '$locationProvider', function($routes, $location) {
 
-  $location.hashPrefix('!');
-
   $routes.when("/", {
-    templateUrl: "javascripts/templates/home.html",
+    templateUrl: "/javascripts/templates/home.html",
     reloadOnSearch: false
   })
     .when("/catalog", {
-      templateUrl: "javascripts/templates/catalog.html",
+      templateUrl: "/javascripts/templates/catalog.html",
       reloadOnSearch: false
     })
     .when("/catalog/:productId", {
-      templateUrl: "javascripts/templates/product.html",
+      templateUrl: "/javascripts/templates/product.html",
       reloadOnSearch: false
     })
+
     .when("/buy", {
-      templateUrl: "javascripts/templates/buy.html",
+      templateUrl: "/javascripts/templates/buy.html",
       reloadOnSearch: false
     })
 
     .when("/delivery", {
-      templateUrl: "javascripts/templates/delivery.html",
+      templateUrl: "/javascripts/templates/delivery.html",
       reloadOnSearch: false
     })
     .when("/payment", {
-      templateUrl: "javascripts/templates/payment.html",
+      templateUrl: "/javascripts/templates/payment.html",
       reloadOnSearch: false
     })
-
     .when("/about", {
-      templateUrl: "javascripts/templates/about.html",
+      templateUrl: "/javascripts/templates/about.html",
       reloadOnSearch: false
     })
     .when("/contacts", {
-      templateUrl: "javascripts/templates/contacts.html",
+      templateUrl: "/javascripts/templates/contacts.html",
       reloadOnSearch: false
     })
     .when("/info", {
-      templateUrl: "javascripts/templates/info.html",
+      templateUrl: "/javascripts/templates/info.html",
       reloadOnSearch: false
     })
 
     .when("/faq", {
-      templateUrl: "javascripts/templates/faq.html",
+      templateUrl: "/javascripts/templates/faq.html",
       reloadOnSearch: false
     })
     .when("/return_guarantee", {
-      templateUrl: "javascripts/templates/return_guarantee.html",
+      templateUrl: "/javascripts/templates/return_guarantee.html",
       reloadOnSearch: false
     })
 
     .when("/product_compare", {
-      templateUrl: "javascripts/templates/product_compare.html",
+      templateUrl: "/javascripts/templates/product_compare.html",
       reloadOnSearch: false
     })
 
     .when("/search", {
-      templateUrl: "javascripts/templates/search.html",
+      templateUrl: "/javascripts/templates/search.html",
       reloadOnSearch: false
     })
 
     .otherwise({
-      templateUrl: 'javascripts/templates/404.html',
+      templateUrl: '/javascripts/templates/404.html',
       reloadOnSearch: false
     });
+
+    $location.html5Mode(true);
 
 }]).run(function($rootScope){
   $rootScope.domain = "http://white-m.ru";
